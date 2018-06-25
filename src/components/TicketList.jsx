1a1ca12294843dcadfd5c0cab098a8b7338e569a
Dawn Mott and Kayl Eubanks <dawn+kayl@>
2018-06-25 10:56:57 -0700

    Set up dev environment, add new components, create template for future projects

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Tickets";
+
+function TicketList(){
+  return(
+    <Ticket
+      location="3A"
+      names="Kayl and Dawn"
+      issue="Firebase will not save record!"/>
+  );
+}
+
+export default TicketList;
