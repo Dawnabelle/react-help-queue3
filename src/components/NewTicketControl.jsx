059a68efcc60fe5969baeb117861244c0d22b216
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-02 14:33:16 -0700

    Add confirmationQuestions component

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions.jsx';
 
 class NewTicketControl extends React.Component {
 
@@ -7,20 +8,17 @@ class NewTicketControl extends React.Component {
     this.state={
       formVisibileOnPage: false
     };
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(){
-    this.setState({formVisibileOnPage: true});
-    console.log('formVisibleOnPage is currently set to:' + this.state.formVisibileOnPage);
-  }
+  // handleClick(){
+  //   this.setState({formVisibileOnPage: true});
+  //   console.log('formVisibleOnPage is currently set to:' + this.state.formVisibileOnPage);
+  // }
 
   render(){
     return(
-      <div>
-        <p>This is the NewTicketControl component!</p>
-        <strong onClick={this.handleClick}>Click me to change my state!</strong>
-      </div>
+      <ConfirmationQuestions/>
     );
   }
 }
