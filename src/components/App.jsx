95e735f0f7b0b2b72e9c3a58a6c957baefa95e33
Kayl Eubanks <kayleubanks@gmail.com>
2018-07-02 14:07:01 -0700

    Add client side routing, links in header, and newTicketForm component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,17 @@ import Header from './Header';
 import TicketList from './TicketList';
 import MyStyledComponent from './MyStyledComponent';
 import ticket from '../assets/images/ticket.jpg';
+import {Switch, Route} from 'react-router-dom';
+import NewTicketForm from './NewTicketForm.jsx';
 
 function App(){
   return (
     <div>
       <Header/>
-      <TicketList />
-      <MyStyledComponent/>
+      <Switch>
+        <Route exact path='/' component={TicketList}/>
+        <Route path='/newticket' component={NewTicketForm}/>
+      </Switch>
       <img src={ticket}/>
     </div>
   );
