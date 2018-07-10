22d5327f1fb0f67dbe10c716ebbc4029c29cdf74
Dawn Mott and Kayl Eubanks <dawn@>
2018-07-10 10:58:21 -0700

    pls

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,6 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log(props);
     this.state = {
       masterTicketList: {},
       selectedTicket: null
