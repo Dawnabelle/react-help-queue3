7c38310a3dabec1893bfa8312b18e07bbfa15292
Dawn Mott and Kayl Eubanks <dawn+kayl@>
2018-07-10 10:13:35 -0700

    dispatch action

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -19,6 +19,7 @@ function NewTicketForm(props){
       issue: _issue.value,
       timeOpen: new Moment()
     };
+    dispatch(action);
     props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, timeOpen:new Moment()});
     _names.value = '';
     _location.value = '';
