0f50c5db404ddeabb68f09224ffa22bb15afc68b
Dawn Mott and Kayl Eubanks <dawn+kayl@>
2018-07-10 10:16:52 -0700

    unsubscribe store

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,7 @@ const store = createStore(ticketListReducer);
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
 );
+
 const render = (Component) => {
   ReactDOM.render(
     <HashRouter>
