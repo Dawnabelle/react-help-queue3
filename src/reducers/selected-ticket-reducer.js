cf905efb2e5eb500b8078104b4d67ede77c6eacb
Dawn Mott and Kayl Eubanks <kayl@>
2018-07-10 15:16:27 -0700

    write and test state for select ticket

--- src/reducers/selected-ticket-reducer.js
+++ src/reducers/selected-ticket-reducer.js
@@ -1,8 +1,7 @@
 export default (state={}, action) => {
   switch (action.type) {
-    case '':
-
-      break;
+    case 'SELECT_TICKET':
+      return action.ticketId;
     default:
       return state;
   }
